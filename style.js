bb925f7deb323344a503672de46d629a332534a8
Hasan Khan <HKHAN194@GMAIL.COM>
2016-11-13 01:32:49 +0000

    Add files via upload

--- style.js
+++ style.js
@@ -1,12 +1,25 @@
-/*Carousel arrow*/
-var width = $('div.item.active > img').width();
-
-$('#myCarousel').width(width);
-
-$('#myCarousel').on('slide.bs.carousel', function () {
-    var width = $('div.item.active > img').width();
-    $('#myCarousel').width(width);
+/*Carousel */
+$(document).ready(function() {
+ 
+  $("#slider").owlCarousel({
+ 
+      
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
 });
+
+
 
 
 /*POP OVER*/
